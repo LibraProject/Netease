c720d4753ecad22068f1607b801ed29e7a9cabc8
ex78 <774421703@qq.com>
2019-09-29 17:14:03 +0800

    'c'

--- src/views/address/index.vue
+++ src/views/address/index.vue
@@ -42,14 +42,27 @@
     </div>
     <div class="addMeta" @click="add">+ 添加地址</div>
     <div :class="['addressPos',{'addresanmit':flag}]">
-        <div class="addresHead">添加地址</div>
-        <div class="addresWrap">
-
+      <div class="addresHead">添加地址</div>
+      <div class="addresWrap">
+        <div class="addressIpt">
+          <input type="text" name="name" placeholder="姓名" v-model="obj.name" />
         </div>
-        <div class="addresFooter">
-          <button class="cancel" @click="cacel">取消</button>
-          <button class="save">保存</button>
+        <div class="addressIpt">
+          <input type="text" name="mobile" placeholder="电话号码" v-model="obj.mobile" />
         </div>
+        <div>北京市/北京市/东城区</div>
+        <div class="addressIpt">
+          <input type="text" name="address" placeholder="详细地址" v-model="obj.address" />
+        </div>
+        <div>
+          设置默认地址
+          <input name="is_default" type="checkbox" v-model="obj.is_default" />
+        </div>
+      </div>
+      <div class="addresFooter">
+        <button class="cancel" @click="cacel">取消</button>
+        <button class="save">保存</button>
+      </div>
     </div>
   </div>
 </template>
@@ -61,17 +74,23 @@ export default {
   data() {
     return {
       title: "地址管理",
-      flag:false
+      flag: false,
+      obj:{
+        name:'',
+        mobile:'',
+        address:'',
+        is_default:false
+      }
     };
   },
   components: { headers },
   methods: {
     ...mapActions("address", ["getAddressList"]),
-    add(){
-      this.flag = true
+    add() {
+      this.flag = true;
     },
-    cacel(){
-      this.flag = false
+    cacel() {
+      this.flag = false;
     }
   },
   computed: {
@@ -102,9 +121,8 @@ export default {
   }
   .main {
     flex: 1;
-   
     padding: 0.1rem;
-     padding-top: 0.55rem;
+    padding-top: 0.55rem;
     .collectItem {
       width: 100%;
       display: flex;
@@ -131,64 +149,83 @@ export default {
       }
     }
   }
-   
-   .addMeta {
+
+  .addMeta {
+    width: 100%;
+    height: 0.45rem;
+    background: rgb(63, 172, 235);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+  }
+  .addMeta:active {
+    background: rgb(9, 127, 206);
+  }
+  .addressPos {
+    position: absolute;
+    left: 100%;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    z-index: 22;
+    transition: all 0.5s linear;
+    .addresHead {
+      width: 100%;
+      height: 0.45rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      box-shadow: 0px 0px 13px 0px #ccc;
+    }
+    .addresWrap {
+      flex: 1;
+      padding: 0.1rem 0;
+      div{
+        height: .45rem;
+        line-height: .45rem;
+          text-indent: 0.2rem;
+          border-bottom: 1px solid #ccc;
+      }
+      .addressIpt {
         width: 100%;
         height: 0.45rem;
-        background: rgb(63, 172, 235);
         display: flex;
         align-items: center;
         justify-content: center;
-        color: white;
-    }
-    .addMeta:active {
-        background: rgb(9, 127, 206);
-    }
-    .addressPos{
-        position: absolute;
-        left:100%;
-        top:0;
-        width: 100%;
-        height: 100%;
-        background:white;
-        display: flex;
-        flex-direction: column;
-        z-index: 22;
-        transition: all .5s linear;
-        .addresHead{
-          width: 100%;
-          height: .45rem;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          box-shadow: 0px 0px 13px 0px #ccc;
+        input {
+          width: 90%;
+          height: 95%;
+          border: 0;
+          outline: none;
         }
-        .addresWrap{
-          flex: 1;
-        }
-        .addresFooter{
-          width: 100%;
-          height: .45rem;
-          display: flex;
-          button{
-            flex: 1;
-            border: 0;
-            outline: 0;
-          }
-          .cancel{
-            background: #ccc;
-          }
-          .save{
-            background:#72adde;
-          }
-          .save:active{
-            background:#2d96ec;
-          }
-        }
+      }
     }
-    .addresanmit{
-      left:0
+    .addresFooter {
+      width: 100%;
+      height: 0.45rem;
+      display: flex;
+      button {
+        flex: 1;
+        border: 0;
+        outline: 0;
+      }
+      .cancel {
+        background: #ccc;
+      }
+      .save {
+        background: #72adde;
+      }
+      .save:active {
+        background: #2d96ec;
+      }
     }
-
+  }
+  .addresanmit {
+    left: 0;
+  }
 }
 </style>
