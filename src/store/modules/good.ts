7c2f2833281c85f0ecf2306c9efe39f6757a9e1a
LHY-Ann <lhy136890@163.com>
2019-09-29 17:14:44 +0800

    collect

--- src/store/modules/good.ts
+++ src/store/modules/good.ts
@@ -1,4 +1,4 @@
-import { goodDetail, goodLook, commit } from '../../service'
+import { goodDetail, goodLook, collectAdd } from '../../service'
 
 export default {
     namespaced: true,
@@ -12,7 +12,8 @@ export default {
         commentData:{},
         img:'',
         numCar:0,
-        productList:{}
+        productList:{},
+        start: false
     },
     mutations: {
         setGood(state:any,payload:any){
@@ -39,24 +40,38 @@ export default {
         setNumCar(state:any,payload:any){
             state.numCar+=payload
             console.log(state.numCar)
+        },
+        setAdd(state:any,payload:any){
+            // 判断收藏颜色
+            if (payload.type === 'add') {
+                state.info.is_app_exclusive=1
+            } else {
+                state.info.is_app_exclusive=0
+            }
+            console.log(payload)
         }
     },
     actions: {
         async getGood({ commit }: any, payload: object) {
             let result = await goodDetail(payload)
-            console.log(result.data,1111)
+            // console.log(result.data,1111)
             commit('setGood',result.data)
         },
         async getGoodLook({ commit }: any, payload: object) {
             let result = await goodLook(payload)
             commit('setGoodLook',result.data)
-            // console.log(result.data,1111)
+            // console.log(result.data,2222)
         },
         async getCommit({ commit }: any, payload: object) {
             console.log(payload)
             let result = await commit(payload)
             // commit('setGoodLook',result.data)
             // console.log(result.data,1111)
+        },
+        async getCollectAdd({ commit }: any, payload: any) {
+            let result = await collectAdd(payload)
+            // console.log(payload,'-----------add')
+            commit('setAdd', result.data)
         }
     }
 }
